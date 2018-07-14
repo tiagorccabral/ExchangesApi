3766c6e1f314d4921e2ea0d1b96aacbbf7db86a4
Tiago Cabral <150150296@aluno.unb.br>
2018-07-14 00:36:48 -0300

    Criado app para adicionar segurança as chaves de usuário.

--- ExchangeApi/settings.py
+++ ExchangeApi/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+from my_secrets import secrets
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '_o+oz_ksx8!55v&m=p8-jp6bx#ac*41v2zjdv$272m^u!2(nbp'
+SECRET_KEY = secrets.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -40,7 +41,10 @@ INSTALLED_APPS = [
 
     # Created apps
     'api',
-    'features'
+    'features',
+    
+    # Third-party libraries
+    'django_secrets'
 ]
 
 MIDDLEWARE = [
