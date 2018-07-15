349d32f97eab1f879bf1d7895183e6de29018678
Tiago Cabral <150150296@aluno.unb.br>
2018-07-15 00:27:46 -0300

    Criadas rotas para app de orderbooks, e primeira view para fazer requisição http.

--- features/urls.py
+++ features/urls.py
@@ -1,9 +1,10 @@
-from django.urls import path
+from django.urls import path, include
 
 from features.views import HomeView
 
 app_name = 'features'
 
 urlpatterns = [
-    path('', HomeView.as_view())
+    path('', HomeView.as_view()),
+    path('orderbooks/', include('features.orderbooks.urls'), name='get_order_book')
 ]
