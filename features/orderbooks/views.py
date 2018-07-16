15983e2649599c1232fc8703117f3df2c561f708
Tiago Cabral <150150296@aluno.unb.br>
2018-07-16 20:58:59 -0300

    Melhorando view das requests.

--- features/orderbooks/views.py
+++ features/orderbooks/views.py
@@ -1,10 +1,24 @@
 import requests
 from django.shortcuts import render
 from django.views import View
+from my_secrets import secrets
 
 
 class RequestOrderBookView(View):
 
     def get(self, request):
-        response = requests.get("https://www.bitstamp.net/api/v2/order_book/btcusd/").json()
-        return render(request, "features/orderbooks/orderbooks.html", {'response': response})
+
+        # Set headers and urls
+        headers = {'Authorization': "ApiToken {}".format(secrets.BIT_COIN_TRADE_TOKEN)}
+        bitcointradeurl = "https://api.bitcointrade.com.br/v1/market?currency=BTC"
+        bitstrampurl = "https://www.bitstamp.net/api/v2/order_book/btcusd/"
+
+        # Makes requisitions
+        bitstampResponse = requests.get(bitstrampurl).json()
+        bitcointradeResponse = requests.get(bitcointradeurl, headers=headers)
+
+        # Return data to template
+        return render(request, "features/orderbooks/orderbooks.html", {
+            'bitstampResponse': bitstampResponse,
+            'bitcointradeResponse': bitcointradeResponse
+        })
