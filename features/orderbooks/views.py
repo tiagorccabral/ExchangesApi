27184a991ec48aa8973f550af75eabebbb6e25ff
Tiago Cabral <150150296@aluno.unb.br>
2018-07-18 15:14:29 -0300

    Adiciona bitcointrade Ethereum aos dados no banco (bid e asks).

--- features/orderbooks/views.py
+++ features/orderbooks/views.py
@@ -5,7 +5,8 @@ from django.views import View
 from my_secrets import secrets
 from orderbooks.models import (
     BitcointradeBitcoinBid, BitcointradeBitcoinAsk,
-    BitstampBitcoinBid, BitstampBitcoinAsk
+    BitstampBitcoinBid, BitstampBitcoinAsk,
+    BitcointradeEthereumsBid, BitcointradeEthereumsAsk
 )
 
 
@@ -18,9 +19,18 @@ class RequestOrderBookView(View):
         bitcointradeurl = "https://api.bitcointrade.com.br/v1/public/BTC/orders"
         bitstrampurl = "https://www.bitstamp.net/api/v2/order_book/btcusd/"
 
-        # Makes requisitions
+        bitcointradeurlETH = "https://api.bitcointrade.com.br/v1/public/ETH/orders"
+        bitstampurlETH = "https://www.bitstamp.net/api/v2/order_book/ethusd/"
+
+        # Makes requisitions for bitcoins
+
         bitcointradeResponse = requests.get(bitcointradeurl).json()
         bitstampResponse = requests.get(bitstrampurl).json()
+
+        # Makes requisitions for ethereums
+
+        bitcointradeETHResponse = requests.get(bitcointradeurlETH).json()
+        bitstampurlETHResponse = requests.get(bitstampurlETH).json()
 
         # Sets a current time for all the information collected by the responses
         current_date = datetime.datetime.now()
@@ -54,6 +64,22 @@ class RequestOrderBookView(View):
             second_coin_value=bitstampResponse['asks'][1][0],
             first_amount=bitstampResponse['asks'][0][1],
             second_amount=bitstampResponse['asks'][1][1],
+            saved_at=current_date,
+        )
+
+        BitcointradeEthereumsBid.objects.create(
+            first_coin_value=bitcointradeETHResponse['data']['bids'][0]['unit_price'],
+            second_coin_value=bitcointradeETHResponse['data']['bids'][1]['unit_price'],
+            first_amount=bitcointradeETHResponse['data']['bids'][0]['amount'],
+            second_amount=bitcointradeETHResponse['data']['bids'][1]['amount'],
+            saved_at=current_date,
+        )
+
+        BitcointradeEthereumsAsk.objects.create(
+            first_coin_value=bitcointradeResponse['data']['asks'][0]['unit_price'],
+            second_coin_value=bitcointradeResponse['data']['asks'][1]['unit_price'],
+            first_amount=bitcointradeResponse['data']['asks'][0]['amount'],
+            second_amount=bitcointradeResponse['data']['asks'][1]['amount'],
             saved_at=current_date,
         )
 
