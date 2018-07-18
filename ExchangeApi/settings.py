2f7ec6322703828ac12864abdc992755a09da751
Tiago Cabral <150150296@aluno.unb.br>
2018-07-17 23:38:47 -0300

    Adicionado app orderbooks a lista de apps criados. Configurado timezone do projeto para São Paulo/Brasil

--- ExchangeApi/settings.py
+++ ExchangeApi/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     # Created apps
     'api',
     'features',
+    'orderbooks',
     
     # Third-party libraries
     'django_secrets'
@@ -114,7 +115,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
